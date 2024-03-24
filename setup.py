58ca9d74dbd3dcf11e99a85158bebe575a45f3f6
Yaopeng Ma <sdumyp@126.com>
2024-03-24 13:03:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='DCNN_SHHS',
     packages=find_packages(),
-    version='0.1.2',
+    version='0.1.3',
     description='Dialated Convolutional Neural Network for sleep stage classification on SHHS data',
     install_requires=['numpy', 'h5py']
 )
